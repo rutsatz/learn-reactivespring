a74cc5edb25d8f6d79681a13cdfb12f3348f7353
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-04-14 10:12:33 -0300

    19. Transforming a Reactive Stream using flatMap.

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
@@ -2,6 +2,7 @@ package com.learnreactivespring.fluxandmonoplayground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -56,6 +57,88 @@ public class FluxAndMonoTransformTest {
 
         StepVerifier.create(namesFlux)
                 .expectNext("JENNY")
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap() {
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F"))
+                /* O flatmap é um caso de uso especial para quando precisamos passar os elementos para
+                 * um serviço externo, como uma chamada ao banco de dados ou uma API externa.
+                 * Nesse exemplo aqui, ele vai dar um Flux<String>. */
+                .flatMap(s -> {
+                    return Flux.fromIterable(converToList(s));
+                })
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    private List<String> converToList(String s) {
+        /* Simula a chamada a um banco. */
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s, "newValue");
+    }
+
+    @Test
+    public void transformUsingFlatMap_usingParallel() {
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F"))
+                /* Ao invés de passar um elemento por vez, ele espera por n elementos e passa todos por vez.
+                 * Ele retorna um Flux<Flux<String>>. */
+                .window(2)
+                /* O flatmap é um caso de uso especial para quando precisamos passar os elementos para
+                 * um serviço externo, como uma chamada ao banco de dados ou uma API externa.
+                 * Nesse exemplo aqui, ele vai dar um Flux<String>. */
+                .flatMap(s -> {
+                    /* Return um Flux<List<String>>. E executa em paralelo. O subscribeOn que troca
+                     * a execução de uma thread para várias e o parallel faz com que as
+                     * operações sejam executadas em paralelo. A partir desse momento, ele muda
+                     * o contexto de execução para outras threads.*/
+                    return s.map(this::converToList).subscribeOn(Schedulers.parallel());
+                })
+                /* Converte novamente para um Flux<String> */
+                .flatMap(s -> Flux.fromIterable(s))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap_parallel_maintain_order() {
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F"))
+                /* Ao invés de passar um elemento por vez, ele espera por n elementos e passa todos por vez.
+                 * Ele retorna um Flux<Flux<String>>. */
+                .window(2)
+                /* O concatMap faz o mesmo que o flatMap, só que mantém a ordem dos elementos. Mas
+                 * nesse cenário que a chamada externa demora 1 segundos, voltamos ao problema de
+                 * demorar 6 segundos para executar tudo, mesmo que em threads separadas. */
+//                .concatMap(s -> {
+                /* Para resolver o problema do concatMap, usamos o flatMapSequential que executa em
+                * paralelo mantendo a ordem. */
+                .flatMapSequential(s -> {
+                    /* Return um Flux<List<String>>. E executa em paralelo. O subscribeOn que troca
+                     * a execução de uma thread para várias e o parallel faz com que as
+                     * operações sejam executadas em paralelo. A partir desse momento, ele muda
+                     * o contexto de execução para outras threads.*/
+                    return s.map(this::converToList).subscribeOn(Schedulers.parallel());
+                })
+                /* Converte novamente para um Flux<String> */
+                .flatMap(s -> Flux.fromIterable(s))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
                 .verifyComplete();
     }
 
