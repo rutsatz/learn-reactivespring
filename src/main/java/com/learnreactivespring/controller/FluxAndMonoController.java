90040db9f1993ec6c95e6ccabd3c5330c0bd0007
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-04-14 16:44:40 -0300

    29. Build the Simple Non Blocking API.

---
+++ src/main/java/com/learnreactivespring/controller/FluxAndMonoController.java
@@ -0,0 +1,38 @@
+package com.learnreactivespring.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+public class FluxAndMonoController {
+
+    /**
+     * Por padrão, o browser espera um JSON, então ele vai esperar receber o json completo para renderizar.
+     *
+     * @return
+     */
+    @GetMapping("/flux")
+    public Flux<Integer> returnFlux() {
+        return Flux.just(1, 2, 3, 4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+    /**
+     * Então, para evitar esse comportamento bloqueante do browser, precisamos colocar no header do response
+     * que estamos retornando um stream.
+     *
+     * @return
+     */
+    @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> returnFluxStream() {
+        return Flux.just(1, 2, 3, 4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+}
