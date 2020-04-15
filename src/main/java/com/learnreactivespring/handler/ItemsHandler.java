515b1401e40112d0b475ab414b8ab379536c7bec
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-04-15 17:04:09 -0300

    64. Create an Item - Functional Endpoint.

--- src/main/java/com/learnreactivespring/handler/ItemsHandler.java
+++ src/main/java/com/learnreactivespring/handler/ItemsHandler.java
@@ -3,6 +3,7 @@ package com.learnreactivespring.handler;
 import com.learnreactivespring.document.Item;
 import com.learnreactivespring.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -39,4 +40,14 @@ public class ItemsHandler {
                 .switchIfEmpty(notFound);
     }
 
+    public Mono<ServerResponse> createItem(ServerRequest serverRequest) {
+        Mono<Item> itemToBeInserted = serverRequest.bodyToMono(Item.class);
+        return itemToBeInserted.flatMap(item ->
+                ServerResponse.status(HttpStatus.CREATED)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        /* Envia a resposta para o cliente, com o item no corpo da requisição. */
+                        .body(itemReactiveRepository.save(item), Item.class)
+        );
+
+    }
 }
--- src/main/java/com/learnreactivespring/router/ItemsRouter.java
+++ src/main/java/com/learnreactivespring/router/ItemsRouter.java
@@ -10,8 +10,7 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static com.learnreactivespring.constants.ItemConstants.ITEM_FUNCTIONAL_END_POINT_V1;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 public class ItemsRouter {
@@ -23,7 +22,9 @@ public class ItemsRouter {
                 .route(GET(ITEM_FUNCTIONAL_END_POINT_V1).and(accept(MediaType.APPLICATION_JSON)),
                         itemsHandler::getAllItems)
                 .andRoute(GET(ITEM_FUNCTIONAL_END_POINT_V1 + "/{id}").and(accept(MediaType.APPLICATION_JSON)),
-                        itemsHandler::getOneItem);
+                        itemsHandler::getOneItem)
+                .andRoute(POST(ITEM_FUNCTIONAL_END_POINT_V1).and(accept(MediaType.APPLICATION_JSON)),
+                        itemsHandler::createItem);
     }
 
 }
