0beae70d35a14df495d659a63d4e64784913cecc
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-04-15 14:53:07 -0300

    56. Create an Item EndPoint.

--- src/main/java/com/learnreactivespring/controller/v1/ItemController.java
+++ src/main/java/com/learnreactivespring/controller/v1/ItemController.java
@@ -7,9 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -39,6 +37,12 @@ public class ItemController {
                 .map(item -> new ResponseEntity<>(item, HttpStatus.OK))
                 /* Se não achou nada, retorna 404. */
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(ItemConstants.ITEM_END_POINT_V1)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Item> createItem(@RequestBody Item item) {
+        return itemReactiveRepository.save(item);
     }
 
 }
--- src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -114,6 +115,23 @@ public class ItemControllerTest {
         webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"), "DEF")
                 .exchange()
                 .expectStatus().isNotFound();
+    }
+
+    @Test
+    public void createItem() {
+        Item item = new Item(null, "Iphone X", 999.99);
+
+        webTestClient.post().uri(ItemConstants.ITEM_END_POINT_V1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(item), Item.class)
+                .exchange()
+                .expectStatus().isCreated()
+                /* Acessa a resposta. */
+                .expectBody()
+                /* Valida os dados. */
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.description").isEqualTo("Iphone X")
+                .jsonPath("$.price").isEqualTo(999.99);
     }
 
 }
