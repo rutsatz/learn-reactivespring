c48532b3ea91113cace29304bc59af68ab34bddb
Rafael Fernando Rutsatz <rafael.rutsatz@compasso.com.br>
2020-04-15 12:06:49 -0300

    51. Get All Items.

---
+++ src/main/java/com/learnreactivespring/controller/v1/ItemController.java
@@ -0,0 +1,24 @@
+package com.learnreactivespring.controller.v1;
+
+import com.learnreactivespring.constants.ItemConstants;
+import com.learnreactivespring.document.Item;
+import com.learnreactivespring.repository.ItemReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@Slf4j
+public class ItemController {
+
+    @Autowired
+    private ItemReactiveRepository itemReactiveRepository;
+
+    @GetMapping(ItemConstants.ITEM_END_POINT_V1)
+    public Flux<Item> getAllItems() {
+        return itemReactiveRepository.findAll();
+    }
+
+}
